314460d569411d1485f37dbdaabe86d8ae189acb
barrtek02 <barrtek02@gmail.com>
2024-04-10 21:28:59 +0200

    refactor: Add black formating.

--- Program.py
+++ Program.py
@@ -10,8 +10,8 @@ import pandas as pd
 class FurnitureApp:
     def __init__(self, master):
         self.master = master
-        self.lower_cabinets = self.load_cabinets('Szafki dolne.xlsx')
-        self.upper_cabinets = self.load_cabinets('Szafki górne.xlsx')
+        self.lower_cabinets = self.load_cabinets("Szafki dolne.xlsx")
+        self.upper_cabinets = self.load_cabinets("Szafki górne.xlsx")
         self.cabinets = {**self.lower_cabinets, **self.upper_cabinets}
         self.cart = {}
         self.entry = None
@@ -21,9 +21,13 @@ class FurnitureApp:
     def load_cabinets(self, filename):
         df = pd.read_excel(filename)
         # group the DataFrame by the big table key column and convert each group to a dictionary
-        grouped = df.groupby('Nazwa').apply(lambda x: x[
-            x.columns.intersection(['partName', 'height', 'width', 'pieces', 'wrapping', 'comments'])].to_dict(
-            'records'))
+        grouped = df.groupby("Nazwa").apply(
+            lambda x: x[
+                x.columns.intersection(
+                    ["partName", "height", "width", "pieces", "wrapping", "comments"]
+                )
+            ].to_dict("records")
+        )
         cabinets = grouped.to_dict()
         return cabinets
 
@@ -38,22 +42,40 @@ class FurnitureApp:
 
         def split(a, n):
             k, m = divmod(len(a), n)
-            return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+            return (
+                a[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n)
+            )
 
-        column_1_low, column_2_low, column_3_low = split(list(self.lower_cabinets.keys()), 3)
-        column_1_upp, column_2_upp, column_3_upp = split(list(self.upper_cabinets.keys()), 3)
+        column_1_low, column_2_low, column_3_low = split(
+            list(self.lower_cabinets.keys()), 3
+        )
+        column_1_upp, column_2_upp, column_3_upp = split(
+            list(self.upper_cabinets.keys()), 3
+        )
 
         def create_buttons(column, column_frame):
             for cabinet in column:
                 cabinet_frame = tk.Frame(column_frame, padx=10, pady=5)
                 cabinet_frame.pack(side=tk.TOP, fill=tk.X, expand=True)
-                cabinet_label = tk.Label(cabinet_frame, text=cabinet, font=("Arial", 12))
+                cabinet_label = tk.Label(
+                    cabinet_frame, text=cabinet, font=("Arial", 12)
+                )
                 cabinet_label.pack(side=tk.LEFT, padx=5)
-                plus_button = tk.Button(cabinet_frame, text="+", font=("Arial", 12), width=2,
-                                        command=lambda selected=cabinet: self.add_to_cart(selected))
+                plus_button = tk.Button(
+                    cabinet_frame,
+                    text="+",
+                    font=("Arial", 12),
+                    width=2,
+                    command=lambda selected=cabinet: self.add_to_cart(selected),
+                )
                 plus_button.pack(side=tk.RIGHT, padx=5)
-                minus_button = tk.Button(cabinet_frame, text="-", font=("Arial", 12), width=2,
-                                         command=lambda selected=cabinet: self.remove_from_cart(selected))
+                minus_button = tk.Button(
+                    cabinet_frame,
+                    text="-",
+                    font=("Arial", 12),
+                    width=2,
+                    command=lambda selected=cabinet: self.remove_from_cart(selected),
+                )
                 minus_button.pack(side=tk.RIGHT, padx=5)
 
         parts_frame_low = tk.Frame(notebook, padx=10, pady=10)
@@ -83,8 +105,8 @@ class FurnitureApp:
         column_3_upp_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         create_buttons(column_3_upp, column_3_upp_frame)
 
-        notebook.add(parts_frame_low, text='Szafki dolne')
-        notebook.add(parts_frame_upp, text='Szafki górne')
+        notebook.add(parts_frame_low, text="Szafki dolne")
+        notebook.add(parts_frame_upp, text="Szafki górne")
 
         cart_frame = tk.Frame(self.master, padx=10, pady=10)
         cart_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
@@ -105,10 +127,14 @@ class FurnitureApp:
         self.cart_listbox.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         # create print button for cart and add it to the cart frame
-        print_button = tk.Button(cart_frame, text="Zapisz", font=("Arial", 12),
-                                 command=lambda cabinets=self.cabinets, entry_button=cart_entry,
-                                                color_entry=color_entry: self.save_cart(
-                                     cabinets, entry_button, color_entry))
+        print_button = tk.Button(
+            cart_frame,
+            text="Zapisz",
+            font=("Arial", 12),
+            command=lambda cabinets=self.cabinets, entry_button=cart_entry, color_entry=color_entry: self.save_cart(
+                cabinets, entry_button, color_entry
+            ),
+        )
         print_button.pack(side=tk.BOTTOM, pady=10, anchor=tk.SE)
 
     def add_to_cart(self, part):
@@ -142,30 +168,34 @@ class FurnitureApp:
         paragraph = doc.add_paragraph(user_entry_color)
         run = paragraph.runs[-1]
         font = run.font
-        font.name = 'Arial'
+        font.name = "Arial"
         font.size = docx.shared.Pt(20)
         parts_list = []
         # add the contents of the cart to the document
         for cabinet, count in self.cart.items():
             partsTemp = cabinets[cabinet]
             for partTemp in partsTemp:
-                partTemp['pieces'] *= count
+                partTemp["pieces"] *= count
                 parts_list.append(partTemp)
         parts_list_sorted = []
 
         for part in parts_list:
             found = False
             for existing_part in parts_list_sorted:
-                if part['partName'] == existing_part['partName'] and part['height'] == existing_part['height'] and part[
-                    'width'] == existing_part['width'] and part['comments'] == existing_part['comments'] and part[
-                    'wrapping'] == existing_part['wrapping']:
-                    existing_part['pieces'] += part['pieces']
+                if (
+                    part["partName"] == existing_part["partName"]
+                    and part["height"] == existing_part["height"]
+                    and part["width"] == existing_part["width"]
+                    and part["comments"] == existing_part["comments"]
+                    and part["wrapping"] == existing_part["wrapping"]
+                ):
+                    existing_part["pieces"] += part["pieces"]
                     found = True
                     break
             if not found:
                 parts_list_sorted.append(part)
 
-        parts_list_sorted.sort(key=lambda x: x['partName'])
+        parts_list_sorted.sort(key=lambda x: x["partName"])
 
         def zero_to_str(wrapping):
             if wrapping == 0:
@@ -174,7 +204,8 @@ class FurnitureApp:
 
         for part in parts_list_sorted:
             doc.add_paragraph(
-                f"{part['partName']}          {part['height']} X {part['width']}            {part['pieces']}szt       {zero_to_str(part['wrapping'])}        {zero_to_str(part['comments'])}")
+                f"{part['partName']}          {part['height']} X {part['width']}            {part['pieces']}szt       {zero_to_str(part['wrapping'])}        {zero_to_str(part['comments'])}"
+            )
         # save the document to a file
         filename = user_entry + ".docx"
         doc.save(filename)
